44c95e6f7ea9387a6673fbe3230cdb50256574bd
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-18 11:28:14 -0500

    button in progress

--- src/components/Doc/Doc.jsx
+++ src/components/Doc/Doc.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom"
 import "./Doc.css"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../contexts/UserIdContext"
+import { Dropdown } from "react-bootstrap"
 
-export const Doc = ({docInfo}) => {
+export const Doc = ({ docInfo }) => {
     const [bodyPreview, setBodyPreview] = useState("")
+    const { userId } = useContext(UserContext)
 
-    useEffect(()=>{
+    useEffect(() => {
         const preview = docInfo.body.slice(0, 75) + "..."
         setBodyPreview(preview)
     }, [docInfo])
 
     return (
         <div className="doc-info">
-            <div>
+            <div className="department-div">
                 {/* if favortie, icon goes here */}
                 <h2>{docInfo.department.name}</h2>
-                {/* if it is your doc options button goes here */}
-                </div>
-                <h1>{docInfo.title}</h1>
-                <Link to={`/profile/${docInfo.userId}`}>{docInfo.user.fullName}</Link>
-                <h3>{bodyPreview}</h3>
+                {
+                    docInfo.userId === userId &&
+                    <Dropdown>
+                        <Dropdown.Toggle></Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item as={Link} to={`/edit-doc/${docInfo.id}`}>Edit</Dropdown.Item>
+                            <Dropdown.Item >Delete</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                }
+            </div>
+            <h1>{docInfo.title}</h1>
+            <Link to={`/profile/${docInfo.userId}`}>{docInfo.user.fullName}</Link>
+            <h3>{bodyPreview}</h3>
         </div>
     )
 }
