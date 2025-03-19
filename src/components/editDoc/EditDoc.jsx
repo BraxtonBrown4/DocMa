a2ae958cf0e24b9d50c3236f36815270d755c292
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-19 13:32:31 -0500

    finished title functionality

--- src/components/editDoc/EditDoc.jsx
+++ src/components/editDoc/EditDoc.jsx
@@ -1,41 +1,57 @@
-import { useContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { UserContext } from "../../customReact/contexts/UserIdContext"
 import { getDocById } from "../../services/docsService"
 import { getAllDepartments } from "../../services/departmentService"
 import "./EditDoc.css"
 
 export const EditDoc = () => {
-    const { userId } = useContext(UserContext)
     const { docId } = useParams()
     const [docInfo, setDocInfo] = useState({})
-    const [allDepartments, setAllDepartments] = useState([])
+    const [document, setDocument] = useState({
+        id: 0,
+        departmentId: 0,
+        title: "",
+        userId: 0,
+        body: "",
+        createdDate: 0,
+        editedDate: 0
+    })
+
+    const localeDateStringInfo = { year: 'numeric', month: 'long', day: 'numeric' }
 
     useEffect(() => {
         getDocById(docId).then((res) => {
             setDocInfo(res)
         })
-
-        getAllDepartments().then((res) => {
-            setAllDepartments(res)
-        })
     }, [docId])
+
+    useEffect(() => {
+        const copy = { ...document }
+        copy.id = docInfo.id
+        copy.departmentId = docInfo.departmentId
+        copy.title = docInfo.title
+        copy.userId = docInfo.userId
+        copy.body = docInfo.body
+        copy.createdDate = docInfo.createdDate
+        copy.editedDate = Math.floor(Date.now() / 1000)
+
+        setDocument(copy)
+    }, [docInfo])
+
+    const handleInputChange = (event) => {
+        const copy = { ...document }
+        copy[event.target.id] = event.target.value
+        setDocument(copy)
+    }
 
 
     return (
         <form className="edit-doc-container">
             <div className="document-info">
-                <div>{docInfo?.department?.name}</div>
-                <div>
-                    <div>{docInfo.title}</div>
-                    <input type="text" />
-                    <div>Created On {new Date(docInfo.createdDate * 1000).toLocaleDateString('en-US',
-                        {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric'
-                        }
-                    )}</div>
+                <div className="input-and-date">
+                    <input type="text" value={document.title} id="title" onChange={handleInputChange} required />
+                    <span>Created On {new Date(document.createdDate * 1000).toLocaleDateString('en-US', localeDateStringInfo)}</span>
+                    <span>Edited on {new Date(document.editedDate * 1000).toLocaleDateString('en-US', localeDateStringInfo)}</span>
                 </div>
                 <div>{docInfo.body}</div>
 
