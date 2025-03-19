bab396a3cdf2b2962458a5a35e365c7b36c7bea5
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-19 12:53:42 -0500

    editDoc in progress

--- src/components/editDoc/EditDoc.jsx
+++ src/components/editDoc/EditDoc.jsx
@@ -1,9 +1,46 @@
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { UserContext } from "../../customReact/contexts/UserIdContext"
+import { getDocById } from "../../services/docsService"
+import { getAllDepartments } from "../../services/departmentService"
+import "./EditDoc.css"
 
 export const EditDoc = () => {
+    const { userId } = useContext(UserContext)
     const { docId } = useParams()
+    const [docInfo, setDocInfo] = useState({})
+    const [allDepartments, setAllDepartments] = useState([])
+
+    useEffect(() => {
+        getDocById(docId).then((res) => {
+            setDocInfo(res)
+        })
+
+        getAllDepartments().then((res) => {
+            setAllDepartments(res)
+        })
+    }, [docId])
+
 
     return (
-        <>EDITDOC {docId}</>
+        <form className="edit-doc-container">
+            <div className="document-info">
+                <div>{docInfo?.department?.name}</div>
+                <div>
+                    <div>{docInfo.title}</div>
+                    <input type="text" />
+                    <div>Created On {new Date(docInfo.createdDate * 1000).toLocaleDateString('en-US',
+                        {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        }
+                    )}</div>
+                </div>
+                <div>{docInfo.body}</div>
+
+            </div>
+        </form>
     )
 }
+
