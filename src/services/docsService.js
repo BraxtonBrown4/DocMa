83b9ff974b70749411cba187003e7dab7df51832
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-19 15:13:32 -0500

    created getAllDocs Service

--- src/services/docsService.js
+++ src/services/docsService.js
@@ -51,3 +51,7 @@ export const getFavoritesByUserId = (id) => {
         return Promise.all(allFavorites)
     })
 }
+
+export const getAllDocs = () => {
+    return fetch(`http://localhost:8088/documents?_expand=department&_expand=user`).then((res) => res.json())
+}
