128331d4857ab2b4104e7b2d557c39a0ae47e91e
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-20 09:15:35 -0500

    added a message if no documents

--- src/components/allDocs/AllDocs.jsx
+++ src/components/allDocs/AllDocs.jsx
@@ -8,11 +8,17 @@ export const AllDocs = () => {
     const [allDocs, setAllDocs] = useState([])
     const [deleteId, setDeleteId] = useState(0)
     const { userId } = useContext(UserContext)
+    const [noDocMsg, setNoDocMsg] = useState('')
+
 
     useEffect(() => {
-            getAllDocs().then((res) => {
+        getAllDocs().then((res) => {
+            if (res.length > 0) {
                 setAllDocs(res)
-            })
+            } else {
+                setNoDocMsg("There are no created documents. You should go make some!")
+            }
+        })
     }, [])
 
     useEffect(() => {
@@ -28,6 +34,7 @@ export const AllDocs = () => {
     return (
         <div className="allDocs-container">
             <header>All Docs</header>
+            <h1>{noDocMsg}</h1>
             {allDocs.map(docInfo => {
                 return <Doc key={docInfo.id} docInfo={docInfo} setDeleteId={setDeleteId} deleteId={deleteId} />
             })}
