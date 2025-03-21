71d59eb1c10a2cd90f66df26e60e833f5ff98466
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-21 11:31:31 -0500

    fixed inconsistant naming

--- src/services/userService.js
+++ src/services/userService.js
@@ -14,13 +14,13 @@ export const createUser = (user) => {
   }).then((res) => res.json())
 }
 
-export const getProfileById = (Id) => {
+export const getUserById = (Id) => {
   return fetch(`http://localhost:8088/users/${Id}?_expand=department&_embed=documents`).then((res) =>
     res.json()
   )
 }
 
-export const updateProfile = (updatedProfile) => {
+export const updateUser = (updatedProfile) => {
   return fetch(`http://localhost:8088/users/${updatedProfile.id}`, {
     method: "PATCH",
     headers: {
