95c67739608277b82dda4aabfe3203436b9d2707
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-24 14:28:17 -0500

    finished inputs

--- src/components/editProfile/EditProfile.jsx
+++ src/components/editProfile/EditProfile.jsx
@@ -10,6 +10,12 @@ export const EditProfile = () => {
     const [numDaysEmployed, setNumDaysEmployed] = useState(0)
     const [employmentDate, setEmploymentDate] = useState(0)
     const navigate = useNavigate()
+    const [profile, setProfile] = useState({
+        id: 0,
+        fullName: "",
+        title: "",
+        departmentId: 0
+    })
 
     useEffect(() => {
         getUserById(userId).then((res) => {
@@ -34,14 +40,41 @@ export const EditProfile = () => {
                 }
             )
             setEmploymentDate(dateToLocaleString)
+
+            setProfile({
+                id: userId,
+                fullName: profileInfo.fullName,
+                title: profileInfo.title,
+                departmentId: profileInfo.departmentId
+            })
         }
     }, [profileInfo])
+
+    const handleInputChange = (event) => {
+        const copy = { ...profile }
+        copy[event.target.id] = event.target.value
+        setProfile(copy)
+    }
+
     return (
         <div className="edit-profile-container">
             <form className="edit-profile-form">
-                <h1>{profileInfo.fullName}</h1>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">Full Name</span>
+                    </div>
+                    <input type="text" className="form-control" value={profile.fullName} id="fullName" onChange={handleInputChange} required/>
+                </div>
+
                 <h2>Department: {profileInfo.department?.name}</h2>
-                <h2>Title: {profileInfo.title}</h2>
+
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">Title</span>
+                    </div>
+                    <input type="text" className="form-control" value={profile.title} id="title" onChange={handleInputChange} required/>
+                </div>
+
                 <h2>Documents created: {profileInfo.documents?.length}</h2>
                 <div className="date-info">
                     <h2>Employed on {employmentDate}</h2>
