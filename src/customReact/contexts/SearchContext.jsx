1058e23c1d5abb6b3651ba974ebf715cb2e48cff
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-26 13:32:14 -0500

    finished filter bar functionality

--- src/customReact/contexts/SearchContext.jsx
+++ src/customReact/contexts/SearchContext.jsx
@@ -11,20 +11,29 @@ export const SearchProvider = ({ children }) => {
 
     useEffect(() => {
         const searchArr = searchObj.str.toLowerCase().split('/')
-        searchArr.push(searchObj.depId)
 
         setSearches({ searchArr: searchArr, depId: searchObj.depId})
     }, [searchObj])
 
 
-    const name = (docs) => {
-        const filteredByAuthor = docs.filter(doc => doc.fullName.toLowerCase().includes(searches.searchArr[0]))
+    const searchFilter = (docs) => {
+        const filteredByAuthor = docs.filter(doc => doc.user.fullName.toLowerCase().includes(searches.searchArr[0] || ''))
 
-        return filteredByAuthor
+        const filteredByTitle = filteredByAuthor.filter(doc => doc.title.toLowerCase().includes(searches.searchArr[1] || ''))
+
+        const filteredByBody = filteredByTitle.filter(doc => doc.body.toLowerCase().includes(searches.searchArr[2] || ''))
+
+        const filteredByDepId = filteredByBody.filter(doc => {
+            if (searches.depId === 0 || doc.departmentId === searches.depId) {
+                return doc
+            }
+        })
+
+        return filteredByDepId
     }
 
     return (
-        <SearchContext.Provider value={{ setSearchObj, name }}>
+        <SearchContext.Provider value={{ setSearchObj, searchFilter }}>
             {children}
         </SearchContext.Provider>
     )
