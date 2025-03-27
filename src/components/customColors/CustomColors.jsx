f24b37057955918ae991baedf965894d98fd3244
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-27 10:34:18 -0500

    added css and part of location logic

--- src/components/customColors/CustomColors.jsx
+++ src/components/customColors/CustomColors.jsx
@@ -1,22 +1,66 @@
+import { SketchPicker } from "react-color";
 import "./CustomColors.css"
 import { useState } from "react";
+import { Dropdown } from "react-bootstrap";
 
-export const CustomColors = ({setMenuOpen}) => {
+export const CustomColors = ({ setMenuOpen }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [location, setLocation] = useState('home');
+    const [color, setColor] = useState("ffffff");
+
+    const updateColor = (newColor) => {
+        setColor(newColor.hex)
+    }
 
     return (
         <div>
-            <button className="modal-btn" onClick={() => {setIsOpen(true), setMenuOpen(false)}}>Custom Colors</button>
+            <button className="modal-btn" onClick={() => { setIsOpen(true), setMenuOpen(false) }}>Custom Colors</button>
 
             {isOpen && (
                 <div className="modal-overlay">
                     <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                    <button className="bi bi-x-circle close-btn" onClick={() => setIsOpen(false)}></button>
-                        <h2>Modal Title</h2>
-                        <p>This is a modal content.</p>
+                        <header className="colors-header">
+                            <h2>{location === 'home' ? "Color Schemes" : "Custom Colors Menu"}</h2>
+                            <button className="bi bi-x-circle close-btn" onClick={() => setIsOpen(false)} disabled></button>
+                        </header>
+
+                        <div className="location-container">
+
+                            {
+                                location === 'home' &&
+                                <>
+                                    <button className="bi bi-plus-square new-btn" onClick={() => { setLocation('newColorScheme') }}></button>
+
+                                    <div className="color-schemes">
+                                        <div className="color-scheme">
+                                            
+                                        </div>
+                                        {/* insert color schemes .map here... with component? */}
+                                    </div>
+                                </>
+                            }
+
+                            {
+                                location === 'newColorScheme' &&
+                                <>
+                                    <div className="custom-color-menu">
+
+                                    </div>
+
+                                    <SketchPicker color={color} onChange={updateColor} presetColors={[]} disableAlpha />
+                                </>
+                            }
+                        </div>
                     </div>
                 </div>
             )}
         </div>
     );
 };
+
+{/*
+                        <div className="custom-color-menu">
+
+                        </div>
+
+                        <SketchPicker color={color} onChange={updateColor} presetColors={[]} disableAlpha /> */}
