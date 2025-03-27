730833b1eb86798848c741890fea65f1a2554172
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-27 16:43:38 -0500

    refactored useLightmode into useColorSchemes

---
+++ src/customReact/hooks/colorSchemes/useColorSchemes.jsx
@@ -0,0 +1,37 @@
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../contexts/UserContext"
+import { getUserById, updateUser } from "../../../services/userService"
+import { getColorSchemeById } from "../../../services/colorSchemeService"
+import "./WebPageColors.css"
+
+export const useColorSchemes = () => {
+    const { userId } = useContext(UserContext)
+    const [colorScheme, setColorScheme] = useState(undefined)
+
+    const html = document.documentElement
+    html.className = "web-page-colors"
+
+    useEffect(() => {
+        if (userId > 0) {
+            getUserById(userId).then((res) => {
+                getColorSchemeById(res.colorSchemeId).then((colorScheme) => {
+                    setColorScheme(colorScheme)
+                })
+            })
+        }
+    }, [userId])
+
+    useEffect(() => {
+        if (userId > 0 && colorScheme.id > 0) {
+            updateUser({ id: userId, colorSchemeId: colorScheme.id })
+
+            Object.entries(colorScheme).forEach(([key, value]) => {
+                if (key !== "id" || key !== "userId" || key !== "name") {
+                    document.documentElement.style.setProperty(key, value);
+                }
+            })
+        }
+    }, [colorScheme])
+
+    return [colorScheme, setColorScheme]
+}
